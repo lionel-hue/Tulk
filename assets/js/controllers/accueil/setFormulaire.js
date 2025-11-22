09b33ef0fe608b42a6ddae6bb60a0432ac2b5572
lionel sisso <sissolionel@gmail.com>
2025-11-22 22:13:57 +0100

    we are starting afresh mate

--- assets/js/controllers/accueil/setFormulaire.js
+++
@@ -1,9 +0,0 @@
-export async function setFormulaire()
-{
-    return fetch("/api/user.php")
-    .then( res => res.json() )
-    .then( data => {
-        const formulaire = document.querySelector("#accueilForm")
-        formulaire.getElementsByTagName("img")[0].setAttribute("src", `./assets/images/${data["image"]}`)
-    } )
-}
--- assets/js/controllers/user.js
+++
@@ -1,6 +0,0 @@
-export async function get_user()
-{
-    const res  = await fetch("/api/user.php")
-    
-    return await res.json()
-}
